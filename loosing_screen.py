432b08f2e2810e577d34a05327a56593bd8b3706
veronika <veronika.slobodyan@mail.mcgill.ca>
2025-11-15 16:15:17 -0500

    ..obstacle

--- loosing_screen.py
+++ loosing_screen.py
@@ -1,6 +1,12 @@
 import pygame
 from player import Player
-from obstacle_definition import Obstacle
+
+class Obstacle:
+    def __init__(self, image, x, y,speed):
+        self.image = image
+        self.rect = image.get_rect(topleft=(x,y))    # track image place by putting it on a rect object
+        self.speed = speed
+        self.active = True             # Whether the obstacle is active in the game
 
 
 def check_for_collision(player, obstacle):
