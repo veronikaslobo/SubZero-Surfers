8eedb5efdd33d7a92d0c251de35b23204febdb28
amelielabe <amelie.labeau@mail.mcgill.ca>
2025-11-15 10:30:29 -0500

    we

---
+++ game_base.py
@@ -0,0 +1,25 @@
+import pygame
+import random
+
+# intializing environment
+pygame.init()
+
+# CONSTANTS
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+FPS = 60
+OBSTACLE_WIDTH, OBSTACLE_HEIGHT = 50,50
+PLAYER_WIDTH, PLAYER_HEIGHT = 50,50
+OBSTACLE_SPEED = 5
+
+# COLORS
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+BLUE = (189,228,255)
+PURPLE =(207,169,245)
+YELLOW =(254,234,160)
+
+# game variables
+
+
+# obstacle
+
