8d97ff9001a20d229b551eb4bc9e70f3c844fb08
veronika <veronika.slobodyan@mail.mcgill.ca>
2025-11-14 22:53:33 -0500

    housekeeping

--- v1.py
+++ v1.py
@@ -11,6 +11,17 @@ class Car:
         self.x = x
         self.y = y
 
+# main game
+def main():
+
+# closes the game if the user presses on X
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+
 
 pygame.quit()
 
