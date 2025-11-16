9d4cb098e19aef1c823b6133b5679c0e74092ad3
veronika <veronika.slobodyan@mail.mcgill.ca>
2025-11-15 22:40:10 -0500

    kk

--- loosing_screen.py
+++ loosing_screen.py
@@ -10,6 +10,7 @@ class Obstacle:
         self.image = image
         self.rect = image.get_rect(topleft=(x,y))    # track image place by putting it on a rect object
         self.width = self.rect.width
+        self.height = self.rect.height
         self.speed = speed
         self.active = True  # Whether the obstacle is active in the game
 
